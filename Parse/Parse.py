40ace7e48d0ec8506af29c0c287031643106aa7c
AO.Redko <AO.Redko>
2017-04-20 12:10:53 +0300

    check on pep8, add list comprehension, refactoring code

--- Parse/Parse.py
+++ Parse/Parse.py
@@ -1,105 +1,89 @@
 import re
-from prettytable import PrettyTable
+
 import matplotlib.pyplot as plt
+from prettytable import PrettyTable
 
-my_file = open("test_data.log", "r").readlines()
-hist = re.findall(r'((?<=\"hist\"\: \{).*?(?=},))',str(my_file))
-data= (re.findall(r'(?<=\"data\"\: \[).*?(?=\])', str(hist)))
-bins= re.findall(r'(?<=\"bins\"\: \[).*?(?=\])', str(hist))
+my_file = open("test_data.log", "r")
+lines = my_file.readlines()
+my_file.close()
 
-#add parsing data[] to list_count
-list_count=[]
-for i in data:
-    k=i.split(', ')
-    for j in k:
-        list_count.append(j)
+hist = re.findall(r'((?<=\"hist\"\: \{).*?(?=},))', str(lines))
+data = re.findall(r'(?<=\"data\"\: \[).*?(?=\])', str(hist))
+bins = re.findall(r'(?<=\"bins\"\: \[).*?(?=\])', str(hist))
 
-#add parsing bins[] to list_sla
-list_sla=[]
-for i in bins:
-    k=i.split(', ')
-    for j in k:
-        list_sla.append(j)
+list_requests = [k for j in (i.split(', ') for i in data) for k in j]
+list_sla = [k for j in (i.split(', ') for i in bins) for k in j]
 
-n=len(list_sla)
+n = len(list_requests)
 
-#count requests for every response time in list_sla
-#1)unique response times to s from list_sla
-response_times=[]
-for i in list_sla:
-    if i not in response_times:
-        response_times.append(i)
+# create dictionary 'response_time': 'count requests'.
+d = {}
+for i in range(n):
+    if list_sla[i] in d:
+        d[int(list_sla[i])] += int(list_requests[i])
+    else:
+        d[int(list_sla[i])] = int(list_requests[i])
 
-#2)create dictionary 'response_time': 'count requests'...
-d={}
-for i in response_times:
-    count=0
-    for j in range(n):
-        if i==list_sla[j]:
-           count+=int(list_count[j])
-    d[int(i)]=count
-
-#summ of all requests
-summ=0
+# sum of all requests
+summ = 0
 for i in d:
-    summ+=d[i]
+    summ += d[i]
 
+# print table HTTP codes
 print('HTTP codes')
 table = PrettyTable([" ", "Count", "Percent"])
 table.add_row(['302 - Found', summ, '100%'])
-print (table)
+print(table)
 
-#count percentiles using dictionary -d
+# count percentiles using dictionary -d
 table = PrettyTable([" ", "Count", "Percent", "Quantile"])
 a = list(d.keys())
 a.sort()
 
 print('Response time distribution')
-percentiles={}
+percentiles = {}
 for i in reversed(a):
-    percent = round(d[i]*100/summ,3)
-    amount=0
+    percent = round(d[i] * 100 / summ, 3)
+    amount = 0
     for j in a:
-       if (j<=i):
-           amount+=d[j]
-    percentile=round(amount*100/summ,3)
-    percentiles[i]=percentile
-    #print("%i\t\t%i\t\t%s\t\t%s" % (i/1000, d[i], percent, percentile))
-    table.add_row([i/1000, d[i], percent, percentile])
+        if j <= i:
+            amount += d[j]
+    percentile = round(amount * 100 / summ, 3)
+    percentiles[i] = percentile
+    # print("%i\t\t%i\t\t%s\t\t%s" % (i/1000, d[i], percent, percentile))
+    table.add_row([i / 1000, d[i], percent, percentile])
 print(table)
 
-#count quantiles using dictionary -percentiles
+# count quantiles using dictionary -percentiles
 print("Cumulative quantiles")
-quantiles=[99, 98, 95, 90, 85, 80, 75, 50]
+quantiles = [99, 98, 95, 90, 85, 80, 75, 50]
 
-Quantile={}
-V=percentiles.keys()
+Quantile = {}
+V = percentiles.keys()
 for q in quantiles:
-    v=100
-    response_time=0
+    v = 100
+    response_time = 0
     for p in V:
-        if percentiles[p]>=q:
-            if percentiles[p]<v:
-                v=percentiles[p]
-                response_time=int(p/1000)
-                Quantile[q]=response_time
-    #print("%i%%\t\t%i" % (q, response_time))
+        if percentiles[p] >= q:
+            if percentiles[p] < v:
+                v = percentiles[p]
+                response_time = int(p / 1000)
+                Quantile[q] = response_time
+                # print("%i%%\t\t%i" % (q, response_time))
 
-
-
-#print table Quantile
+# print table Quantile
 table = PrettyTable([" ", "Quantile"])
 for i in reversed(sorted(Quantile.keys())):
     table.add_row(["%i%%" % i, Quantile[i]])
-print (table)
+print(table)
 
-#graph
-x=list(Quantile.keys())
-y=list(Quantile.values())
+# graph
+x = list(Quantile.keys())
+y = list(Quantile.values())
 
 # bar()
 fig = plt.figure()
-plt.bar(x,y)
+plt.bar(x, y)
 plt.title('Simple bar chart')
-plt.grid(True)   # линии вспомогательной сетки
-#plt.show()
+plt.grid(True)  # линии вспомогательной сетки
+# plt.show()
